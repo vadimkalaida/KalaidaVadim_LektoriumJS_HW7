c95d460ed8d90a62fedfa68af1daf3e4556f1f62
VadimKalayda <vadosgta1@gmail.com>
2019-11-26 00:26:23 +0200

    beginning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <h1 className="App-title">My <span>React</span> App</h1>
     </div>
   );
 }
